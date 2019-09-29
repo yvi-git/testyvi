ea4f85269a873f8e9e74e9debae9200ddee02fdf
yvi-git <yvi-git@outlook.fr>
2019-09-29 17:16:35 +0200

    Update comment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //test
+            //test fdgdf
+
             Console.WriteLine("Hello World!");
         }
     }
